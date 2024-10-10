0691561e89a646dd5380ffb75a1d0926f4b60b0d
Andro James <androjamesnervida.wrk@gmail.com>
2024-10-10 16:41:10 +0800

    Add files via upload

---
+++ GameStats.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class 06TaskPerformance
+{
+	public static void main(String[] args);
+	{
+		Scanner input = new Scanner(System.in);
+		int a;
+		int b;
+		int c;
+
+		System.out.println("Enter the base attack speed: ");
+		a = input.nextInt();
+		System.out.println("Enter the bonus attack speed: ");
+		b = input.nextInt();
+		System.out.println("Enter the level: ");
+		c = input.nextInt();
+
+
+	}
+}
